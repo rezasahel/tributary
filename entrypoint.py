ca2c883fe9cbd030c86f4cacfb4da37f7ba5ee2e
Reza Sahel <mrezasahel@gmail.com>
2024-05-02 12:04:24 +0200

    chore: doker compose fixed

--- entrypoint.py
+++ entrypoint.py
@@ -1,32 +1,25 @@
-from flask import Flask, request, jsonify
+
 import json
-import redis
+import redis as redis
+from flask import Flask, request
 from loguru import logger
+
+
+
+HISTORY_LENGTH = 10
+DATA_KEY = "engine_temperature"
 
 app = Flask(__name__)
 
-HISTORY_LENGTH = 10
-DATA_KEY = 'engine_temperature'
 
-def get_current_temperature():
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
-    current_temperature = database.lindex(DATA_KEY, 0)
-    return current_temperature
-
-def calculate_average_temperature():
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
-    temperatures = database.lrange(DATA_KEY, 0, -1)
-    temperatures = [float(temp) for temp in temperatures]
-    average_temperature = sum(temperatures) / len(temperatures) if temperatures else 0
-    return average_temperature
 
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
     payload = request.get_json(force=True)
     logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
 
-    engine_temperature = payload.get('engine_temperature')
-    logger.info(f'engine temperature to record is: {engine_temperature}')
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
 
     database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
     database.lpush(DATA_KEY, engine_temperature)
@@ -34,16 +27,29 @@ def record_engine_temperature():
 
     while database.llen(DATA_KEY) > HISTORY_LENGTH:
         database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+
     logger.info(f"record request successful")
     return {"success": True}, 200
 
-@app.route('/collect', methods=['GET'])
-def collect_engine_temperature():
-    current_temperature = get_current_temperature()
-    average_temperature = calculate_average_temperature()
-    response = {
-        "current_engine_temperature": current_temperature,
-        "average_engine_temperature": average_temperature
-    }
-    return jsonify(response), 200
 
+@app.route('/collect', methods=['POST'])
+def collect_engine_temperature():
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"pulled engine temperature values from redis: {engine_temperature_values}")
+
+    datapoint_sum = 0
+    for data_point in engine_temperature_values:
+        datapoint_sum += float(data_point)
+    average_engine_temperature = datapoint_sum / len(engine_temperature_values)
+    logger.info(f"calculated average engine temperature: {average_engine_temperature}")
+    logger.info(f"returning current engine temperature: {engine_temperature_values[0]}")
+
+    result = {
+        "current_engine_temperature": engine_temperature_values[0],
+        "average_engine_temperature": average_engine_temperature
+    }
+    logger.info(f"collect request successful")
+    return result, 200
